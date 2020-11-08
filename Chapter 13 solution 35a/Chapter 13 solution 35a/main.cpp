7d259ffe488acf412908f5dbd62afb90b15840f5
Charles Best-Zambrana <charles.bestzambrana@gmail.com>
2020-11-08 14:01:29 -0500

    Add files via upload

---
+++ Chapter 13 solution 35a/Chapter 13 solution 35a/main.cpp
@@ -0,0 +1,22 @@
+//including essential header files
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+T surprise(T x, T y)
+{
+	return x + y;
+}
+
+
+int main()
+
+{
+
+	//calling the given template function
+
+	cout << surprise(5, 7) << endl;
+
+}
